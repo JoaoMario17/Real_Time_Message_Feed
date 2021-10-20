7692151ab5b2eeb5969ee73df7e2358eae374f9e
João-Mário <joaomf17p2@gmail.com>
2021-10-20 19:42:54 -0300

    1-dia

---
+++ src/controllers/GetLast3MessageController.ts
@@ -0,0 +1,15 @@
+import { Request, Response} from "express"
+import { GetLast3MessagesService } from "../services/GetLast3MessagesService";
+
+class GetLast3MessageController {
+    async handle(req: Request,res: Response){
+
+        const service = new GetLast3MessagesService();
+
+        const result = await service.execute();
+
+        return res.json(result);
+    }
+}
+
+export { GetLast3MessageController }
---
+++ src/routes.ts
@@ -0,0 +1,18 @@
+import { Router}  from "express"
+import { AuthenticateUserController } from "./controllers/AuthenticationUserController";
+import { CreateMessageController } from "./controllers/CreateMessageController";
+import { ensureAuthenticated } from "./middleware/ensureAuthenticated";
+import { GetLast3MessageController } from "./controllers/GetLast3MessageController";
+import { ProfileUserController } from "./controllers/ProfileUserController";
+
+const router = Router();
+
+router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post("/messages", ensureAuthenticated, new CreateMessageController().handle);
+
+router.get("/messages/last3", new GetLast3MessageController().handle);
+
+router.get("/profile", ensureAuthenticated,new ProfileUserController().handle)
+
+export { router }
